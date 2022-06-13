f3cd75e9e306d3fa4356ae9c8f70b400adc8d432
Rifqi Arkan <rifqiarkan20@gmail,com>
2022-06-13 23:02:24 +0700

    setuponclick

--- app/src/main/java/aplikasi/rifqiarkan/xsportapp/ScreenSport.java
+++ app/src/main/java/aplikasi/rifqiarkan/xsportapp/ScreenSport.java
@@ -2,13 +2,37 @@ package aplikasi.rifqiarkan.xsportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.google.android.material.button.MaterialButton;
 
 public class ScreenSport extends AppCompatActivity {
+
+    public static class BUNDLE{
+        public static String KEY_FUTSAL = "KEY_FUTSAL";
+    }
+
+    LinearLayout buttonFutsal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_sport);
+        String KEY_FROM_LOGIN = getIntent().getStringExtra(screen_futsal.BUNDLE.KEY_LOGIN);
+        initView();
+        setupOnClick();
+    }
+
+    private void setupOnClick() {
+        buttonFutsal.setOnClickListener(view -> {
+
+        });
+    }
+
+    private void initView() {
+        buttonFutsal = findViewById(R.id.icFutsal);
     }
 }
--- app/src/main/java/aplikasi/rifqiarkan/xsportapp/screen_futsal.java
+++ app/src/main/java/aplikasi/rifqiarkan/xsportapp/screen_futsal.java
@@ -2,13 +2,37 @@ package aplikasi.rifqiarkan.xsportapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class screen_futsal extends AppCompatActivity {
 
+    public static class BUNDLE{
+        public static String KEY_LOGIN= "KEY_LOGIN";
+
+    }
+
+    MaterialButton buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_futsal);
+        initView();
+        setupOnClick();
+    }
+
+    private void setupOnClick() {
+        buttonLogin.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ScreenSport.class);
+            intent.putExtra(BUNDLE.KEY_LOGIN, "arkan@gmail.com");
+            startActivity(intent);
+        });
+    }
+
+    private void initView() {
+        buttonLogin = findViewById(R.id.button);
     }
 }
